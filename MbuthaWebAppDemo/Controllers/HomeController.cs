32a648a54e915e0de1e6ebcce378f91e0c6bc43b
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-09 01:36:55 -0400

    Update HomeController.cs
    Setting up the webconfig parameter and creating private config and finally setting up email functionality for the porfolio

--- MbuthaWebAppDemo/Controllers/HomeController.cs
+++ MbuthaWebAppDemo/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using MbuthaWebAppDemo.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Mail;
 using System.Web.Mvc;
+using MailMessage = System.Net.Mail.MailMessage;
 
 namespace MbuthaWebAppDemo.Controllers
 {
@@ -22,11 +28,45 @@ namespace MbuthaWebAppDemo.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            EmailModel model = new EmailModel();
+            return View(model);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold>(){1}</p><p>Message:</p><p>{2}</p>";
+
+                    var from = "Moses Mwangi Protfolio<mosesmwangi1979@gmail.com>";
+
+                    model.Body = "This is a message from my portfolio site. The name and email from the person contacting you is above.";
+
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = "Portfolio Contact Email",
+                        Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+
+                    var svc = new PersonalEmail();
+                    await svc.SendAsync(email);
+
+                    return View(new EmailModel());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+
+            }
+            return View(model);
+        }
         public ActionResult BSDemo()
         {
             return View();
